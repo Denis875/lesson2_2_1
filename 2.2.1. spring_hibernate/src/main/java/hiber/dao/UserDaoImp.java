cc5d2d22d578f5c473d57588c24fba62f9123aa7
Denis875 <73391737+Denis875@users.noreply.github.com>
2024-05-15 21:25:24 +0300

    Constant SQL expression

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,6 +11,9 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
+    private static final String SELECT_USER_BY_CAR = "FROM User AS user INNER JOIN FETCH user.car AS car WHERE car.model=:model AND car.series=:series";
+    private static final String SELECT_USER = "FROM User";
+
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -22,15 +25,14 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(SELECT_USER);
         return query.getResultList();
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsersByCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User AS user INNER JOIN FETCH user.car AS car " +
-                "WHERE car.model=:model AND car.series=:series");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(SELECT_USER_BY_CAR);
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList();
