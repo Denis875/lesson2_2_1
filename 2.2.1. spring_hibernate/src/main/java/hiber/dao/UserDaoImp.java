8b40741b007fcf92ab94d62870e2a7434ac1f5a2
Denis875 <73391737+Denis875@users.noreply.github.com>
2024-05-15 21:18:21 +0300

    Up case SQL code

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -22,15 +22,15 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User");
         return query.getResultList();
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsersByCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User as user inner join fetch user.car as car " +
-                "where car.model=:model and car.series=:series");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("FROM User AS user INNER JOIN FETCH user.car AS car " +
+                "WHERE car.model=:model AND car.series=:series");
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList();
