eed925b62f6e395fa096d229a37bcea2f845b563
denis875 <denisok578@gmail.com>
2024-06-10 11:50:32 +0300

    Constructor autowired

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,7 +2,6 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
@@ -11,8 +10,14 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    private static final String SELECT_USER_BY_CAR = "from User as user inner join fetch user.car as car where car.model=:model and car.series=:series";
+    private static final String SELECT_USER = "from User";
+
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
@@ -22,15 +27,14 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(SELECT_USER);
         return query.getResultList();
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsersByCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User as user inner join fetch user.car as car " +
-                "where car.model=:model and car.series=:series");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(SELECT_USER_BY_CAR);
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList();
